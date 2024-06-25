c0bf2509fe45dba78bd4b3ff9da3dddc8702b8b1
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2024-06-25 15:46:32 +0300

    Доработал выделение

--- src/algorithms/RabinKarp.ts
+++ src/algorithms/RabinKarp.ts
@@ -1,4 +1,4 @@
-{/*function rabinKarp(text: string, pattern: string, prime: number = 101): number[] {
+function rabinKarp(text: string, pattern: string, prime: number = 101): number[] {
     const n = text.length;
     const m = pattern.length;
     const base = 256; // размер алфавита (256 для ASCII)
@@ -30,7 +30,7 @@
                 }
             }
             if (j === m) {
-                result.push(i + 1); // Индексы в TypeScript начинаются с 0, поэтому добавляем 1
+                result.push(i);
             }
         }
 
@@ -44,87 +44,8 @@
     }
 
     return result;
-}*/}
-
-function rabinKarp(text: string, pattern: string, prime: number = 101): [number, number, number][] {
-    const lines = text.split('\n');
-    const result: [number, number, number][] = [];
-    let totalCount = 0;
-    let lineStart = 0;
-
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i].toLowerCase();
-        const n = line.length;
-        const m = pattern.toLowerCase().length;
-        const base = 256; // размер алфавита (256 для ASCII)
-        let patternHash = 0;
-        let lineHash = 0;
-        let h = 1;
-
-        // Вычисление хеш-значения для первых m символов текста
-        for (let j = 0; j < m - 1; j++) {
-            h = (h * base) % prime;
-        }
-
-        // Вычисление хеш-значений для образца и первых m символов текста
-        for (let j = 0; j < m; j++) {
-            patternHash = (base * patternHash + pattern.toLowerCase().charCodeAt(j)) % prime;
-            lineHash = (base * lineHash + line.charCodeAt(j)) % prime;
-        }
-
-        // Поиск совпадений
-        for (let j = 0; j <= n - m; j++) {
-            // Если хеш-значения совпадают, проверяем на точное совпадение
-            if (patternHash === lineHash) {
-                let k;
-                for (k = 0; k < m; k++) {
-                    if (line[j + k] !== pattern.toLowerCase()[k]) {
-                        break;
-                    }
-                }
-                if (k === m) {
-                    result.push([i + 1, j + 1, ++totalCount]); // Индексы в TypeScript начинаются с 0, поэтому добавляем 1
-                }
-            }
-
-            // Вычисление хеш-значения для следующих m символов текста
-            if (j < n - m) {
-                lineHash = (base * (lineHash - line.charCodeAt(j) * h) + line.charCodeAt(j + m)) % prime;
-                if (lineHash < 0) {
-                    lineHash += prime;
-                }
-            }
-        }
-
-        lineStart += line.length + 1; // Добавляем длину строки и символ новой строки
-    }
-
-    return result;
 }
-
-// Пример использования
-const text = "мама папа сестра\nсестра папа мама\nМАМА мама\nмама папа МАМА";
-const pattern = "мама";
-const result = rabinKarp(text, pattern);
-
-console.log(`Всего найдено слов: ${result.length}`);
-for (const [lineNum, pos, count] of result) {
-    console.log(`Строка: ${lineNum}, Позиция: ${pos}, Всего найдено: ${count}`);
-}
-
-
-// Пример использования
-{/*
-const text = "мама папа сестра\nсестра папа мама\nмама мама\nмама папа мама";
-const pattern = "мама";
-const result = rabinKarp(text, pattern);
-
-console.log(`Всего найдено слов: ${result.length}`);
-for (const [lineNum, pos, count] of result) {
-    console.log(`Строка: ${lineNum}, Позиция: ${pos}, Всего найдено: ${count}`);
-}*/}
-
 
 export {
-    rabinKarp
+    rabinKarp,
 }
--- src/workspace.tsx
+++ src/workspace.tsx
@@ -1,47 +1,112 @@
-import React, {MutableRefObject, RefObject, useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {MutableRefObject, useCallback, useEffect, useRef, useState} from 'react'
 import styles from './workspace.module.css'
-import {PopoverWrapper} from "./PopoverWrapper";
-import {rabinKarp, knuthMorrisPratt} from "./algorithms/RabinKarp";
+import {PopoverWrapper} from "./PopoverWrapper"
+import {rabinKarp} from "./algorithms/RabinKarp"
+import {knuthMorrisPratt} from "./algorithms/KnuthMorrisPratt";
+import {boyerMoore} from "./algorithms/BoyerMoore";
 
 const Workspace: React.FC = () => {
+    const [times, setTimes] = useState<number[]>([])
     const [text, setText] = useState<string>('');
-    // const [result, setResult] = useState<number[]>([])
+    const [result, setResult] = useState<number[]>([])
     const inputRef = useRef(null)
     const {
         value: pattern
     } = useInput(inputRef)
 
-
-
-    //const [result, setResult] = useState<[number, string, number][]>([]);
-    const [result, setResult] = useState<[number, number, number][]>([]);
     useEffect(() => {
         if (text.length && pattern.length) {
-            setResult(rabinKarp(text, pattern))
+
         }
     }, [text, pattern])
 
+    const textRef = useRef<HTMLDivElement>(null)
 
+    const makeTextFile = function (text: string) {
+        const data = new Blob([text], {type: 'text/plain'});
+        return window.URL.createObjectURL(data);
+    };
 
-    const textView = useMemo(() => {
-        if (!result.length) {
-            return <div className={styles.text}>{text}</div>
+    const onResultClick = () => {
+        const lines = text.split('\n')
+        const linesData: {start: number, end: number}[] = []
+        lines.forEach((line, i) => {
+            if (!linesData.length) {
+                linesData.push({
+                    start: 0,
+                    end: line.length,
+                })
+            }
+            else {
+                const prev =  linesData[i - 1].end + 1
+                linesData.push({
+                    start: prev,
+                    end: prev + line.length,
+                })
+            }
+        })
+        const data: {line: number, pos: number}[] = result.map(i => {
+            const lineIndex = linesData.findIndex(lineData => lineData.start <= i && lineData.end >= i)
+            return ({
+                line: lineIndex + 1,
+                pos: i - linesData[lineIndex].start
+            });
+        })
+
+        const fileName= './result.txt'
+        const element = document.createElement('a');
+        element.setAttribute('href', makeTextFile(makeResultText(data)));
+        element.setAttribute('download', fileName);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    const makeResultText = (data:  { line: number, pos: number }[]): string => (
+        data.reduce((acc, occurrence, i) => (
+            acc += `${i + 1}. Строка: ${occurrence.line}, позиция: ${occurrence.pos + 1}\n`
+        ), `Всего вхождений: ${data.length}\n\n`)
+    )
+
+    const onClick = useCallback(() => {
+        if (pattern) {
+            const oneLineText = text.replaceAll('\n', ' ').toLowerCase()
+            const newTimes = []
+
+            const rkStart = performance.now()
+            const result = rabinKarp(oneLineText, pattern)
+            newTimes.push(performance.now() - rkStart)
+
+            const kmStart = performance.now()
+            knuthMorrisPratt(oneLineText, pattern)
+            newTimes.push(performance.now() - kmStart)
+
+            const bmStart = performance.now()
+            boyerMoore(oneLineText, pattern)
+            newTimes.push(performance.now() - bmStart)
+
+            setTimes(newTimes)
+
+            setResult(result)
+            if (result.length) {
+                const ranges = result?.map(i => ({start: i, end: i + pattern.length}))
+                if (textRef?.current) {
+                    const highlightedText = highlighter(text, ranges)
+                    textRef.current.innerHTML = highlightedText.replaceAll('\n', '<br/>')
+                }
+            }
+            else {
+                alert('Ничего не найдено')
+            }
         }
+    }, [pattern, text])
 
-        const start = result[0][0] - 1
-        return (
-            <div className={styles.text}>
-                {text.slice(0, start)}
-                <span className={styles.selected}>{pattern}</span>
-                {text.slice(start + pattern.length)}
-            </div>
-        )
-    }, [result, pattern, text])
-
-    // const resultView = result.map(i => <span key={i}>{i}, </span>)
-    const resultView = useMemo(() => {
-        return result.map(([i]) => <span key={i}>{i}, </span>);
-    }, [result]);
+    useEffect(() => {
+        if (textRef?.current) {
+            textRef.current.innerHTML = text.replaceAll('\n', '<br/>')
+        }
+    }, [text, textRef]);
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -49,7 +114,7 @@ const Workspace: React.FC = () => {
             const reader = new FileReader();
             reader.onload = (e) => {
                 const content = e.target?.result as string;
-                setText(content);
+                setText(content.replaceAll('\r\n', '\n').replaceAll('\n\r', '\n').replaceAll('\r', '\n'));
             };
             reader.readAsText(file);
         }
@@ -58,36 +123,38 @@ const Workspace: React.FC = () => {
     return (
         <>
             <div className={styles.workspace}>
-                <input ref={inputRef} type="text"/>
-                {/*Вхождения: {resultView}*/}
-                {result.map(([index, line, count], i) => (
-                    <div key={i}>
-                        <p>Всего найдено слов: {result.length}</p>
-                        <p>Строка: {index}</p>
-                        <p>Позиция в строке: {line}</p>
-                    </div>
-                ))}
-                {/*<select>
-                   <option>Сгенерировать</option>
-                    <option>Еще что-то</option>
-                </select>
-                <select>
-                    <option>Открыть</option>
-                    <option>Консоль</option>
-                </select>*/}
-                <select>
-                    <option>Метод</option>
-                    <option>Рабина-Карпа</option>
-                    <option>Кнута–Морриса–Пратта</option>
-                    <option>Бойера-Мура</option>
-                </select>
-                <button>Результат</button>
-                <button>Анализ</button>
-                <PopoverWrapper title="Помощь" >
-                    <ul>
+                <input className={styles.input} ref={inputRef} type="text"/>
+                <button className={styles.button} onClick={onClick}>Найти</button>
+                <PopoverWrapper title="Помощь">
+                    <ul className={styles.help_list}>
                         <button className={styles.help_buttons}>Алгоритм Рабина-Карпа</button>
+                        Алгоритм поиска строки, который ищет шаблон, то есть подстроку, в тексте, используя хеширование.
+                        Представляет собой модификацию линейного алгоритма. Он основан на весьма простой идее. Вырежем "окошечко" размером M и будем двигать его по тексту. Нас интересует, не совпадает ли слово в "окошечке" с заданным образцом. Сравнивать по всем буквам долго. Вместо этого фиксируем некоторую числовую функцию на словах длины M. Тогда задача сведется к сравнению чисел, что, несомненно, быстрее. Если значения этой функции на слове в "окошечке" и на образце различны, то совпадения нет. Только если значения одинаковы, необходимо проверять последовательно совпадение по буквам.
+
+                        Сложность - O(M+N)
+
+                        Алгоритм Рабина-Карпа применяется для поиска строк по нескольким образцам. В этом случае текущее хеш-значение подстроки эффективным образом сравнивается с множеством хеш-значений образцов.
+                        Алгоритм Рабина-Карпа является алгоритмом с наименьшими предварительными трудозатратами, поэтому он годится для использования при решении некоторого класса задач.
                         <button className={styles.help_buttons}>Алгоритм Кнута–Морриса–Пратта</button>
+                        Алгоритм Кнута – Морриса – Пратта (КМП).
+                        Эффективный алгоритм, осуществляющий поиск подстроки в строке, используя то, что при возникновении несоответствия само слово содержит достаточно информации, чтобы определить, где может начаться следующее совпадение, минуя лишние проверки. Время работы алгоритма линейно зависит от объёма входных данных, то есть разработать асимптотически более эффективный алгоритм невозможно.
+                        Алгоритм более полно использует информацию, полученную во время сканирования. После сдвига образца по тексту стоит вести сравнение посимвольно. Если не совпал первый символ, нет смысла переходить к следующему.
+
+                        Сложность - O(n)
                         <button className={styles.help_buttons}>Алгоритм Бойера-Мура</button>
+                        Алгоритм общего назначения, предназначенный для поиска подстроки в строке. Преимущество этого алгоритма в том, что ценой некоторого количества предварительных вычислений над шаблоном (но не над строкой, в которой ведётся поиск), шаблон сравнивается с исходным текстом не во всех позициях — часть проверок пропускается как заведомо не дающая результата. Имеет нелинейную оценку трудоемкости в худшем случае, но в среднем работает быстрее. Этот алгоритм считается наиболее быстрым среди алгоритмов общего назначения, предназначенных для поиска подстроки в строке.
+
+                        Сложность - O (n+m)
+
+                        Эффективность алгоритма Бойера – Мура в среднем выше по сравнению с алгоритмом КМП, но существенно зависит от длины текста и длины образца. Так при длине строки до 10 символов он показывает себя хуже, даже чем последовательный поиск.
+                    </ul>
+                </PopoverWrapper>
+                <button className={styles.button} onClick={onResultClick}>Результат</button>
+                <PopoverWrapper title="Анализ">
+                    <ul className={styles.list}>
+                        <button className={styles.help_buttons}>Алгоритм Рабина-Карпа: {times[0]} ms</button>
+                        <button className={styles.help_buttons}>Алгоритм Кнута–Морриса–Пратта: {times[1]} ms</button>
+                        <button className={styles.help_buttons}>Алгоритм Бойера-Мура: {times[2]} ms</button>
                     </ul>
                 </PopoverWrapper>
             </div>
@@ -100,11 +167,18 @@ const Workspace: React.FC = () => {
                 onChange={handleFileChange}
                 style={{display: 'none'}}
             />
-            {textView}
+            <div ref={textRef} className={styles.text}></div>
         </>
     )
         ;
 };
+
+function highlighter(str: string, ixs: {start: number, end: number}[]){
+    return ixs.reduceRight((s,ix) =>
+        `${s.slice(0, ix.start)}<span class=${styles.selected}>${s.slice(ix.start, ix.end)}</span>${s.slice(ix.end)}`,
+        str
+    );
+}
 
 const useInput = (ref: MutableRefObject<HTMLInputElement | null>) => {
     const [value, setValue] = useState('')
